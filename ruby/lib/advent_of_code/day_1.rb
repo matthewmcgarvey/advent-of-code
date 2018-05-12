6d7c39a84e865f2d8c51d50f7ed3d2c7971dee1e
Matthew McGarvey <tnraptor14@hotmail.com>
2018-05-12 00:40:48 -0500

    completed day 1 -> ruby

--- ruby/lib/advent_of_code/day_1.rb
+++ ruby/lib/advent_of_code/day_1.rb
@@ -1,6 +1,5 @@
 module AdventOfCode
   class Day1
-
     def self.captcha(captcha)
       total = 0
       digits = captcha.chars
@@ -10,7 +9,22 @@ module AdventOfCode
         .map(&:first)
         .map(&:to_i)
         .reduce(0, &:+)
-    end 
-    
+    end
+
+    # checking for equality half way around is same as comparing each element
+    # of array to itself after it has been split in half and the two pieces swapped
+    # 1221 -> 2112
+    def self.captcha_two(captcha)
+      digits = captcha.chars
+      second = digits.each_slice( (digits.size/2).round )
+        .to_a
+        .reverse
+        .flatten
+      digits.zip(second)
+        .select { |a, b| a == b}
+        .map(&:first)
+        .map(&:to_i)
+        .reduce(0, &:+)
+    end
   end
 end
