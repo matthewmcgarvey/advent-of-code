ae322cc5376534d8684d8965f228a11d2527dcaf
Matthew McGarvey <tnraptor14@hotmail.com>
2018-12-03 20:28:06 -0600

    reworked file structure for simplicity

--- 2018/javascript/lib/day1.js
+++
@@ -1,9 +0,0 @@
-
-function frequencyChanges(changes) {
-  return changes.map(change => {
-    let x = parseInt(change.substr(1));
-    return change.charAt(0) === '-' ? -x : x;
-  }).reduce((a, b) => a + b, 0);
-}
-
-module.exports = { frequencyChanges }
