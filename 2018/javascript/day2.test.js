65a0a4cc6b08fe19d151f76820d3014a96ae8c1e
Matthew McGarvey <tnraptor14@hotmail.com>
2018-12-07 20:13:54 -0600

    day2 part2 done, so confused

--- 2018/javascript/day2.test.js
+++ 2018/javascript/day2.test.js
@@ -73,3 +73,45 @@ test('part1 puzzle', t => {
   let result = checkSum(ids);
   t.is(result, 6422);
 });
+
+// PART 2
+
+function diff(a, b) {
+  return a.split('')
+    .map((c, i) => [c, b.charAt(i)])
+    .filter(pair => pair[0] === pair[1])
+    .map(pair => pair[0])
+    .join('');
+}
+
+function part2(ids) {
+  let diffs = [];
+  for (var a of ids) {
+    for (var b of ids) {
+      let x = {
+        a: a,
+        b: b,
+        diff: diff(a, b)
+      };
+      diffs.push(x);
+    }
+  }
+  let desiredLength = ids[0].length - 1;
+  return diffs.filter(x => x.diff.length == desiredLength)[0]
+}
+
+test('returns matching letters of 2 strings', t => {
+  let result = diff("abcd", "qbcf");
+  t.is(result, "bc");
+});
+
+test('diff compares positionally', t => {
+  let result = diff("abca", "bacd");
+  t.is(result, "c");
+});
+
+test('part2 puzzle', t => {
+  let ids = ["qcsnyvpigkxmrdawlfdefotxbh", "qcsnyvligkymrdawljujfotxbh", "qmsnyvpigkzmrnawzjuefotxbh", "qosnyvpigkzmrnawljuefouxbh", "qcsnhlpigkzmrtawljuefotxbh", "qcsnyvpigkzmrdapljuyfotxih", "qcsnbvpiokzmrdawljuerotxbh", "qcfnyvmigkzmrdawljuefotdbh", "qcsnynpigkzmrdawljuefptxbp", "qcsgyapigkzmrdawljuafotxbh", "qcsnyvpigkzmrdapljueeotibh", "qcfnyvpigkzmndawljuwfotxbh", "qzsayvpigkzmrdawijuefotxbh", "qcsnsvpiekzmrdawljfefotxbh", "ncsnyvpigkzmrdaaljuefotxzh", "qssnyvpigkzmrdawljuefotobg", "qcshyipigkzmrdajljuefotxbh", "qcsnyvtigkzmrdawljgeaotxbh", "qcsnkvpxgkzmrdawljuefltxbh", "qcsnyvpiikzmrdawljuwfoqxbh", "qcsnybpigwzmqdawljuefotxbh", "qcsiyvpipkzbrdawljuefotxbh", "qldnyvpigkzmrdzwljuefotxbh", "qcsnyvpwgkzcrdawljuefmtxbh", "qcsnyvnigkzmrdahmjuefotxbh", "qcsnydpigkzmrdazljuefotxnh", "qcsqyvavgkzmrdawljuefotxbh", "ucsnyvpigkzmrdawljuefocxwh", "qcsnivpigrzmrdawljuefouxbh", "tcsnyvpibkzmrdawlkuefotxbh", "qcstytpigkzmrdawsjuefotxbh", "qcynyvpigkzmrdawlluefotjbh", "qcstyvpigkqrrdawljuefotxbh", "icsnyvpizkzmrcawljuefotxbh", "qcsnyvpimkzmrdavljuezotxbh", "qvsnoupigkzmrdawljuefotxbh", "qcsnyvpigkzmrdawwjuefftxgh", "qcpnyvpijkzmrdvwljuefotxbh", "qcsnyvpigkzmxdakdjuefotxbh", "jcsvyvpigkqmrdawljuefotxbh", "qcwnyvpigczmrsawljuefotxbh", "qcsnyvpdgkzmrdawljuefoixbm", "qysnyvpigkzmrdmwljuefotxbp", "qcsnavpigkzmrdaxajuefotxbh", "qcsfkvpigkzmrdawlcuefotxbh", "qcsnyvpigkvmrdawljcefotpbh", "qcsnyvpiqkkmrdawlvuefotxbh", "qhsnyvpigkzmrdawnjuedotxbh", "qasnlvpigkzmrdawljuefotxkh", "qgsnyvpigkzmrdabpjuefotxbh", "jcsnyvdigkzmrmawljuefotxbh", "qcsnivpigkzmrdawljuefonxth", "qcsnyjpigkzmrdawljgefotxmh", "qcstyvpigkzmrdacljuefovxbh", "qcsnvvpigkzmrdawljuewotrbh", "qcsnyvaigdzmrdawljueuotxbh", "qcsnyvpegkzmwdawljzefotxbh", "qcsnevpngkzmrdawlouefotxbh", "qcsnuvpigozmrdawljuefotdbh", "qgsnyvpigkzmqdayljuefotxbh", "qcsnyvpigkzmrdcwdjuofotxbh", "qcnnyvpigkzmrzawljuefstxbh", "qlsgyvpigkzmrdtwljuefotxbh", "qcsnyfpigkzlroawljuefotxbh", "qcsnkvwigkzmrdowljuefotxbh", "qcsnrvpigkzmrdawljuvfltxbh", "qcsnyvpigkzvreawljuefotxmh", "qcsrgvpigkzmrdawliuefotxbh", "qysnyvpigkzmrdawlxaefotxbh", "qcsnyvpigizmrdlwljuefotxbi", "qzsnyvpitkzmrdawljuefbtxbh", "qzgnyvpigkzmrdawljuefotxih", "qcsnyvpigkzmrdawlguefvtxbb", "qcsnyvpigkzmidawljuefouxjh", "qksnyvpigkzmrdawlruefotxhh", "qcsnyvpinkzmrdaaljuefotxah", "qcsnxvpigkzjrdawljuefhtxbh", "qcsnyvpigkzardawlgueuotxbh", "qcsnyvpiakzmrdpwljuefotxbt", "qcsnyvpigkzmrdawkjuefotxgb", "qcsnyvpigkzmrdawljuehocsbh", "qcsnsvpigktmrdawljuefotxvh", "qusnrvpigkzrrdawljuefotxbh", "qcsnyhiigkzmrdawrjuefotxbh", "qcsnavpigkzmrdawlfuefotxbz", "qcsnyvpigkzmmdamsjuefotxbh", "qcsnyvzigkzmrdcwljmefotxbh", "qcsnyvpigkzmriawljuefotbbe", "qcsnyvpigksmrdawljaefotxbd", "qcsnyvpigkzfrdawljuefoxxmh", "qcsnyvpygkrmrdawljuefotxbi", "qcsngvwigfzmrdawljuefotxbh", "qcsnyvpigkmkrdauljuefotxbh", "qcsnyvpigxzmrdgwljuefwtxbh", "qconyapigkzmrdaxljuefotxbh", "qcsnydpigkzwrdawljulfotxbh", "qcsnyvpimkzmmdawljuefotxch", "qcsnkspigkzmrdawgjuefotxbh", "qcsnyvpigkzmrdhwljfefbtxbh", "qcsnyipijkztrdawljuefotxbh", "qcseyvpigkrhrdawljuefotxbh", "qcsnyvpivkzmrdawljuefottbb", "qcsnyvpigkzmrdawlouefcjxbh", "qcsnyvpigkzmrgayljuefotxbm", "qcsnyvpvgkzmrdawrjujfotxbh", "qcsnyvpigkzmndawljuefqtxch", "qcsnyvpigbzmrdawljuefotibg", "qcsnyvpigkzmseawljuefotxbv", "qcsnwvpigkzmraawnjuefotxbh", "mcsnyvpiqkzmrdawljuefotlbh", "bcsnyvpigczmrdmwljuefotxbh", "qcsnyvpigkzmrtawljuegntxbh", "qcsnyvpijkzmrdawlmrefotxbh", "qdsnyvpfgkzmrdawljuekotxbh", "qcsnyvpigkzmrdawcjfegotxbh", "qcslyvphgkrmddawljuefotxbh", "qcsnyvpigkzmsdawkjuefojxbh", "qzsnyvpigkzmrzawljuefmtxbh", "qcsnyvpqgkzmcdawljuefttxbh", "qcsnyvpbgkpmrdawljuefoqxbh", "qcsnyvpigkemrdywljmefotxbh", "qcsnyypigkzmrdawljmefotxwh", "jcsnyvhwgkzmrdawljuefotxbh", "qcsnyvpigkzmrdawljurlotxwh", "qcsnnvpigzzmrdawljuefotwbh", "hcsnyvpigkzmrdarljuefitxbh", "qcsnyvpilkzmrfawljuefotsbh", "qcsnynpigkzmldawijuefotxbh", "qcsnyvpkgkjmrdawljuefotxlh", "qcsnylpigkzprdawljgefotxbh", "qcsnyvpigkzmrrawljnefohxbh", "qcsnivpigkzmrqawlbuefotxbh", "qcsgyvpigkzmrfawljuefotbbh", "qccuyvpigkzmrdawyjuefotxbh", "gcsnyvpigkzjrdawljuefotxby", "qcsmyvpiekzbrdawljuefotxbh", "qcsnyvpzgkrmrdawljuefotxbs", "qesnyvpigkzmpdqwljuefotxbh", "qcsnyvpigqzmrdawljuefutibh", "qcdnyvpigkzirdawljfefotxbh", "qcsnyvpiukzmrcrwljuefotxbh", "qcsnbvpickzmrdswljuefotxbh", "qcsnyvpighzmrpadljuefotxbh", "qccnyvpigkzmrdawljudxotxbh", "qcsnyvpigkzmrdabljuesotxlh", "qcsnyvpigkzmrrawlruefozxbh", "qconyzpigkzmrdawljuefotjbh", "qclnyvpigkzmrdxwljuefotbbh", "qcsnygpigkzmrdawlhuefooxbh", "qcsnyvpigkzmvdawljuefntxnh", "qcskyvpigkzmreawljuefotubh", "qrsnyvpxgkzmrdawljuefotxbz", "qclnyvpigtamrdawljuefotxbh", "qcsnyvpigkzmrdawojxefoyxbh", "qcsnyvpinkzmrdakljuwfotxbh", "qcsnyvpiykzmedawljuefgtxbh", "qcsayvpigkcmrdawijuefotxbh", "qcsnyvuiekzmrdamljuefotxbh", "qcdnyvpigkzmrdawnjuefoxxbh", "qcsnfvpwgszmrdawljuefotxbh", "qcsnycpigkzmrdawljqefotxih", "qcslyvphgkrmrdawljuefotxbh", "ecsnyvpigkzmrdawykuefotxbh", "qcsayvpigkzmraawljuekotxbh", "qcsnyvpigkdmrdawljuewofxbh", "qcznyvpigkzqrdawljuefotxnh", "qcsnyvplgkzmrdawljiefotlbh", "qcsnyvpigkzmroewljuefotbbh", "qcvnyvpigkzvrdawujuefotxbh", "qcanyypigkzmrdaeljuefotxbh", "qcsnyvwigkzmrdewljuefotxqh", "qcsryvpigkvmrdawljuefotabh", "pcsnyvpigkwmrdawljueforxbh", "qcsncvpigkzmrdawljuefotwmh", "qcsnyvpigozmrdawljudfozxbh", "qcsnynpigkzmrbawhjuefotxbh", "qcsnyvuigkzmrqawljuefotxch", "qcsnyvpickzmrdawljueeofxbh", "qcsnyvpigkzgrdawljueiouxbh", "qcsnyvpigkztrdawljuxnotxbh", "qcsnyvpigwzvrdawljfefotxbh", "qcsnyvpilkzmrdawljuefotxcz", "qcsnjvpigkzmrdawljuefoywbh", "qhsnyvpigyzmrdawljuhfotxbh", "qcsnyvpirkzmfdawljuffotxbh", "qcsjyvpigkzmvdawljuefotxzh", "qcszivpirkzmrdawljuefotxbh", "qwsnyvpigkzmtdawljuefetxbh", "qcrntvpigkzordawljuefotxbh", "qrsnyvpigkzmsdawljrefotxbh", "qcsnyviivkzmrdazljuefotxbh", "ecsnyvpigkzmrdawyjuefotxbw", "qnsnyvpkgkzmrdawljueqotxbh", "qcsyyppigkzmrdawljuefotxba", "qcsnyvpigkzhrdpwljuefouxbh", "ucsnyvpigkzmrdawojuefouxbh", "qysnyvpigkzmrdawljukfotxbd", "qcjnyvpigkzmrdalljfefotxbh", "fcsnyapigkmmrdawljuefotxbh", "qcnnkvpigkzmrdawljuefctxbh", "ocsnyvpigkzmsdawljuefotxbl", "qcsnyvpiakomrdawpjuefotxbh", "qcsnyvpigkzmrdawljvefbtxwh", "qcsnuvpigkzmvdfwljuefotxbh", "qcsnyapihkzmrdagljuefotxbh", "qzsnyvpigkzmrdawtjuefotxgh", "qcsnyvpigkzmrdawljuefomyah", "ocsnyvpigkzqrdawljuefotxbt", "qnsnyvpigkzmrdawljvevotxbh", "icsnyvpigkzmrdawljuefntxbt", "qcsnyvpigkzdrdawljuefotbbm", "scsnyvpigkzmrgawljuofotxbh", "qcsnydpigkzmrdowljuefotkbh", "qcsnyvtikkzmrdawljuefolxbh", "qcsiyvpigkcmrddwljuefotxbh", "qyrnyvpigkzmodawljuefotxbh", "pcsndvpfgkzmrdawljuefotxbh", "qcsnyvkigkhmriawljuefotxbh", "qcsnyvpigkzmsdmwlkuefotxbh", "dosnyvpigkzmrdawdjuefotxbh", "qcnnnvpigkzmrdzwljuefotxbh", "qcsnyvpivkumrdailjuefotxbh", "qcsnyvpigkzmrdswljuzfotxbz", "qcscynpigkzmrdawljuefotxbc", "qeanyvpigkzmrdawijuefotxbh", "qclnylpigkzmrdawljuefotxyh", "qcsnyvpigkzmrdawljbefowxbp", "qcsnyvpagkzmrdawljuefolebh", "qxsiyvpigkzmrdawljuefotxgh", "qcsnyvpigkynrdawljuefoqxbh", "qcsnevpigkzmrdxwgjuefotxbh", "qcsnyvpdgkzlrdawljeefotxbh", "qcsnyvpigkzmrgawljxbfotxbh", "ecsnyvpigkzmrdbwbjuefotxbh", "qcsnyvpigkzmraawujuefocxbh", "qcsnyvpihkzmrdawljuefouxbn", "fgsqyvpigkzmrdawljuefotxbh", "qcsnyvpigkmmrdawajuefotnbh", "qcsnyvvigkzmrdahljudfotxbh", "qcsnyvpixkzmrdqwljutfotxbh", "ncsnyvpickzmrdawljuehotxbh", "qcsnyvpizkzmrdawlpuefotxbp", "wcsnyvfigkzmrdakljuefotxbh", "qcsnyvpigkznrdhwljupfotxbh", "jcsnyvpigkpmzdawljuefotxbh", "qcsnyppigkkmrdawljujfotxbh", "qcsnyvpigkumrdaeljuefodxbh", "qcsnyvhigkzmrdrwljuefodxbh", "qcsnyvpigkacrdawtjuefotxbh", "qcsnyvpigkzmylawlquefotxbh"];
+  let result = part2(ids);
+  t.is(result.diff, 'qcslyvphgkrmdawljuefotxbh');
+});
