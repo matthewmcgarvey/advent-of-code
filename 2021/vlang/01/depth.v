e7d260c7d66c03527e1da7f0b0b966750fee7419
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-12-01 17:03:50 -0600

    vlang - completed day 1

---
+++ 2021/vlang/01/depth.v
@@ -0,0 +1,25 @@
+import os
+import arrays
+
+fn part_a(entries []int) int {
+	mut count := 0
+	for i, entry in entries {
+		if i == 0 {
+			continue
+		} else if entry > entries[i - 1] {
+			count++
+		}
+	}
+	return count
+}
+
+fn part_b(entries []int) ?int {
+	variations := arrays.window(entries, size: 3).map(arrays.sum(it) ?)
+	return part_a(variations)
+}
+
+fn main() {
+	lines := os.read_lines('./input.txt') ?
+	entries := lines.map(it.int())
+	println(part_b(entries) ?)
+}
