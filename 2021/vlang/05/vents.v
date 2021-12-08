6040c68c947432bbe2b95fa34417c80ea6bf8813
matthewmcgarvey <matthewmcgarvey14@gmail.com>
2021-12-08 15:34:21 -0600

    Solved Day 5 Part 2 in V

--- 2021/vlang/05/vents.v
+++ 2021/vlang/05/vents.v
@@ -7,15 +7,21 @@ struct Point {
 	y int
 }
 
-fn get_range(x int, y int) []int {
-	from := if x > y { y } else { x }
-	to := if x > y { x } else { y }
-	mut temp := []int{}
-	for i in from .. to {
-		temp << i
+fn advance_point(a int, b int) int {
+	if a == b {
+		return a
+	} else if a > b {
+		return a - 1
+	} else {
+		return a + 1
 	}
-	temp << to
-	return temp
+}
+
+fn move_one(from Point, to Point) Point {
+	new_x := advance_point(from.x, to.x)
+	new_y := advance_point(from.y, to.y)
+
+	return Point{new_x, new_y}
 }
 
 fn part_a(lines []string) {
@@ -28,11 +34,18 @@ fn part_a(lines []string) {
 			continue
 		}
 
-		range := if begin_point.x != end_point.x {
-			get_range(begin_point.x, end_point.x).map(Point{it, begin_point.y})
-		} else {
-			get_range(begin_point.y, end_point.y).map(Point{begin_point.x, it})
+		mut range := []Point{}
+		range << begin_point
+		for {
+			last := range.last()
+			next := move_one(last, end_point)
+			if last == next {
+				break
+			} else {
+				range << next
+			}
 		}
+
 		for point in range {
 			temp := "${point.x},${point.y}"
 			if !(temp in count) {
@@ -51,11 +64,43 @@ fn part_a(lines []string) {
 }
 
 fn part_b(lines []string) {
-	println('PART B:')
+	mut count := map[string]int{}
+	for line in lines {
+		str_range := line.split('->').map(it.trim_space().split(',').map(it.int()))
+		begin_point := Point{str_range[0][0], str_range[0][1]}
+		end_point := Point{str_range[1][0], str_range[1][1]}
+
+		mut range := []Point{}
+		range << begin_point
+		for {
+			last := range.last()
+			next := move_one(last, end_point)
+			if last == next {
+				break
+			} else {
+				range << next
+			}
+		}
+
+		for point in range {
+			temp := "${point.x},${point.y}"
+			if !(temp in count) {
+				count[temp] = 0
+			}
+			count[temp] += 1
+		}
+	}
+	mut total := 0
+	for _, val in count {
+		if val > 1 {
+			total += 1
+		}
+	}
+	println('PART B: $total')
 }
 
 fn main() {
-	lines := os.read_lines('./example_input.txt') ?
+	lines := os.read_lines('./input.txt') ?
 	part_a(lines)
 	part_b(lines)
 }
